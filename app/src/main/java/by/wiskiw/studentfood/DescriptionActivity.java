4e3fe08f491092caca3cf4d30520436b016082bc
Andrew Yablonskiy <dron2310@gmail.com>
2018-11-19 23:19:33 +0300

    Add desctiption activity

--- app/src/main/java/by/wiskiw/studentfood/DescriptionActivity.java
+++ app/src/main/java/by/wiskiw/studentfood/DescriptionActivity.java
@@ -1,7 +1,8 @@
 package by.wiskiw.studentfood;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class DescriptionActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class DescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
--- app/src/main/java/by/wiskiw/studentfood/MainActivity.kt
+++ app/src/main/java/by/wiskiw/studentfood/MainActivity.kt
@@ -1,5 +1,6 @@
 package by.wiskiw.studentfood
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        startActivity(Intent(this, DescriptionActivity::class.java))
     }
 }
