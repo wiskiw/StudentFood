5ba2ff09a931f93eda34c30aba84d74e49cb84bb
Andrew Yablonskiy <dron2310@gmail.com>
2018-11-21 23:24:46 +0300

    Complete recipe UI

--- app/src/main/java/by/wiskiw/studentfood/DescriptionActivity.java
+++ app/src/main/java/by/wiskiw/studentfood/DescriptionActivity.java
@@ -1,17 +1,102 @@
 package by.wiskiw.studentfood;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import by.wiskiw.studentfood.entity.CookLevel;
 
 public class DescriptionActivity extends AppCompatActivity {
+
+    private LinearLayout cookingLevelsLl;
+
+    private ImageView headerIv;
+    private ImageView action1Iv;
+    private ImageView action2Iv;
+    private ImageView action3Iv;
+
+    private TextView descriptionTv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        cookingLevelsLl = findViewById(R.id.linear_layout_cooking_levels);
+        headerIv = findViewById(R.id.image_view_header);
+
+        action1Iv = findViewById(R.id.image_view_action_1);
+        action2Iv = findViewById(R.id.image_view_action_2);
+        action3Iv = findViewById(R.id.image_view_action_3);
+
+        descriptionTv = findViewById(R.id.text_view_description);
+
+
+        setupToolbar();
+
+
+        debug();
+    }
+
+    private void debug() {
+        setupToolbarTitle("Debug Блюдо!");
+        setDescription("\n" +
+                "Один из самых популярных десертов в мире — брауни — был придуман в 1893 году на кухне легендарного отеля Palmer House в Чикаго. ");
+
+        CookLevel level1 = new CookLevel();
+        level1.setText("Доведите воду до кипения");
+        level1.setTimeMin(7);
+        addCookLevel(level1);
+
+
+        CookLevel level2 = new CookLevel();
+        level2.setText("Нарежте сыр");
+        level2.setTimeMin(3);
+        addCookLevel(level2);
+
+        CookLevel level3 = new CookLevel();
+        level3.setText("Всыпьте сыра");
+        level3.setTimeMin(1);
+        addCookLevel(level3);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupToolbarTitle(@NotNull String title) {
+        ActionBar toolbar = getSupportActionBar();
+        if (toolbar != null) {
+            toolbar.setTitle(title);
+        }
+    }
+
+    private void setDescription(@NotNull String description) {
+        descriptionTv.setText(description);
+    }
+
+    private void addCookLevel(CookLevel cookLevel) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View itemView = inflater.inflate(R.layout.content_cook_level, cookingLevelsLl, false);
+
+        TextView textTv = itemView.findViewById(R.id.text_view_text);
+        TextView timeTv = itemView.findViewById(R.id.text_view_time);
+        ImageView imageView = itemView.findViewById(R.id.image_view_image);
+
+        textTv.setText(cookLevel.getText());
+        timeTv.setText(cookLevel.getTimeString());
+        // todo : show image imageView.set...
+
+        cookingLevelsLl.addView(itemView);
     }
 }
---
+++ app/src/main/java/by/wiskiw/studentfood/entity/CookLevel.java
@@ -0,0 +1,39 @@
+package by.wiskiw.studentfood.entity;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CookLevel {
+
+    private String text = "";
+    private Long time = 0L;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public void setTimeMin(int minute) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(GregorianCalendar.MINUTE, minute);
+        this.time = calendar.getTimeInMillis();
+    }
+
+    public String getTimeString() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(time);
+        return calendar.get(GregorianCalendar.MINUTE) + " min";
+    }
+
+}
