e1848ffcd902f8a2d45c9727b0a19c0205b88ba8
Yablonsky, Andrey <ayablonsky@qulix.com>
2018-12-06 14:30:30 +0300

    Finish time utils

--- app/src/main/java/by/wiskiw/studentfood/utils/DateUtils.kt
+++ app/src/main/java/by/wiskiw/studentfood/utils/DateUtils.kt
@@ -1,10 +1,14 @@
 package by.wiskiw.studentfood.utils
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 object DateUtils {
 
-    fun format(timeMs: Long, format: String): String {
-        // TODO formatTime(timeMs: Long, format: String): String
-        return timeMs.toString()
+    fun format(timeMs: Long, pattern: String) = format(Date(timeMs), pattern)
 
+    fun format(date: Date, pattern: String): String {
+        val simpleDateFormat = SimpleDateFormat(pattern, Locale.getDefault())
+        return simpleDateFormat.format(date)
     }
 }
